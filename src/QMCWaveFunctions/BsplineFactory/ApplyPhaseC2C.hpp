b2c339cf857e9c48184fd7c7c7e240b8e83fc99a
Rohit Goswami <rgoswami@ieee.org>
2026-07-22 08:37:30 -0500

    SplineC2C: share scalar C2C phase kernels via ApplyPhaseC2C

--- src/QMCWaveFunctions/BsplineFactory/ApplyPhaseC2C.hpp
+++ src/QMCWaveFunctions/BsplineFactory/ApplyPhaseC2C.hpp
@@ -16,6 +16,88 @@ namespace qmcplusplus
 {
 namespace C2C
 {
+inline void complex_index_bounds(size_t first_real,
+                                 size_t last_real,
+                                 size_t num_orbitals,
+                                 size_t& first_complex,
+                                 size_t& last_complex)
+{
+  first_complex = first_real / 2;
+  last_complex  = omptarget::min(last_real / 2, num_orbitals);
+}
+
+template<typename ST>
+inline void phase_sincos(ST x, ST y, ST z, ST kX, ST kY, ST kZ, ST& s, ST& c)
+{
+  const ST phase_xy = std::fma(y, kY, x * kX);
+  const ST phase    = -std::fma(z, kZ, phase_xy);
+  omptarget::sincos(phase, &s, &c);
+}
+
+template<typename ST, typename TT>
+inline TT apply_phase(ST s, ST c, ST value_r, ST value_i)
+{
+  return TT(std::fma(-s, value_i, c * value_r), std::fma(s, value_r, c * value_i));
+}
+
+template<typename ST, typename TT>
+inline void apply_phase_vgl(ST x,
+                            ST y,
+                            ST z,
+                            ST val_r,
+                            ST val_i,
+                            ST g0_r,
+                            ST g0_i,
+                            ST g1_r,
+                            ST g1_i,
+                            ST g2_r,
+                            ST g2_i,
+                            ST lcart_r,
+                            ST lcart_i,
+                            const ST G[9],
+                            ST kX,
+                            ST kY,
+                            ST kZ,
+                            ST mKK,
+                            TT& psi,
+                            TT& dpsi_x,
+                            TT& dpsi_y,
+                            TT& dpsi_z,
+                            TT& d2psi)
+{
+  constexpr ST two(2);
+  const ST &g00 = G[0], &g01 = G[1], &g02 = G[2], &g10 = G[3], &g11 = G[4], &g12 = G[5], &g20 = G[6], &g21 = G[7],
+           &g22 = G[8];
+
+  ST s;
+  ST c;
+  phase_sincos(x, y, z, kX, kY, kZ, s, c);
+
+  const ST dX_r = g00 * g0_r + g01 * g1_r + g02 * g2_r;
+  const ST dY_r = g10 * g0_r + g11 * g1_r + g12 * g2_r;
+  const ST dZ_r = g20 * g0_r + g21 * g1_r + g22 * g2_r;
+
+  const ST dX_i = g00 * g0_i + g01 * g1_i + g02 * g2_i;
+  const ST dY_i = g10 * g0_i + g11 * g1_i + g12 * g2_i;
+  const ST dZ_i = g20 * g0_i + g21 * g1_i + g22 * g2_i;
+
+  const ST gX_r = dX_r + val_i * kX;
+  const ST gY_r = dY_r + val_i * kY;
+  const ST gZ_r = dZ_r + val_i * kZ;
+  const ST gX_i = dX_i - val_r * kX;
+  const ST gY_i = dY_i - val_r * kY;
+  const ST gZ_i = dZ_i - val_r * kZ;
+
+  const ST lap_r = lcart_r + mKK * val_r + two * (kX * dX_i + kY * dY_i + kZ * dZ_i);
+  const ST lap_i = lcart_i + mKK * val_i - two * (kX * dX_r + kY * dY_r + kZ * dZ_r);
+
+  psi     = apply_phase<ST, TT>(s, c, val_r, val_i);
+  d2psi   = apply_phase<ST, TT>(s, c, lap_r, lap_i);
+  dpsi_x  = apply_phase<ST, TT>(s, c, gX_r, gX_i);
+  dpsi_y  = apply_phase<ST, TT>(s, c, gY_r, gY_i);
+  dpsi_z  = apply_phase<ST, TT>(s, c, gZ_r, gZ_i);
+}
+
 template<typename ST, typename TT>
 inline void assign_v(ST x,
                      ST y,
@@ -33,13 +115,12 @@ inline void assign_v(ST x,
   const ST* restrict val = offload_scratch_ptr;
   TT* restrict psi       = results_scratch_ptr;
 
-  //phase
-  ST s, c, p = -(x * kx[index] + y * ky[index] + z * kz[index]);
-  omptarget::sincos(p, &s, &c);
-
   const ST val_r = val[index * 2];
   const ST val_i = val[index * 2 + 1];
-  psi[index]     = TT(val_r * c - val_i * s, val_i * c + val_r * s);
+  ST s;
+  ST c;
+  phase_sincos(x, y, z, kx[index], ky[index], kz[index], s, c);
+  psi[index] = apply_phase<ST, TT>(s, c, val_r, val_i);
 }
 
 /** assign_vgl
@@ -58,10 +139,6 @@ inline void assign_vgl(ST x,
                        size_t myKcart_padded_size,
                        int index)
 {
-  constexpr ST two(2);
-  const ST &g00 = G[0], &g01 = G[1], &g02 = G[2], &g10 = G[3], &g11 = G[4], &g12 = G[5], &g20 = G[6], &g21 = G[7],
-           &g22 = G[8];
-
   const ST* restrict k0 = myKcart_ptr;
   const ST* restrict k1 = myKcart_ptr + myKcart_padded_size;
   const ST* restrict k2 = myKcart_ptr + myKcart_padded_size * 2;
@@ -81,41 +158,15 @@ inline void assign_vgl(ST x,
   const ST val_r = val[jr];
   const ST val_i = val[ji];
 
-  //phase
-  ST s, c, p = -(x * kX + y * kY + z * kZ);
-  omptarget::sincos(p, &s, &c);
-
-  //dot(PrimLattice.G,myG[j])
-  const ST dX_r = g00 * g0[jr] + g01 * g1[jr] + g02 * g2[jr];
-  const ST dY_r = g10 * g0[jr] + g11 * g1[jr] + g12 * g2[jr];
-  const ST dZ_r = g20 * g0[jr] + g21 * g1[jr] + g22 * g2[jr];
-
-  const ST dX_i = g00 * g0[ji] + g01 * g1[ji] + g02 * g2[ji];
-  const ST dY_i = g10 * g0[ji] + g11 * g1[ji] + g12 * g2[ji];
-  const ST dZ_i = g20 * g0[ji] + g21 * g1[ji] + g22 * g2[ji];
-
-  // \f$\nabla \psi_r + {\bf k}\psi_i\f$
-  const ST gX_r = dX_r + val_i * kX;
-  const ST gY_r = dY_r + val_i * kY;
-  const ST gZ_r = dZ_r + val_i * kZ;
-  const ST gX_i = dX_i - val_r * kX;
-  const ST gY_i = dY_i - val_r * kY;
-  const ST gZ_i = dZ_i - val_r * kZ;
-
-  const ST lap_r = lcart[jr] + mKK_ptr[index] * val_r + two * (kX * dX_i + kY * dY_i + kZ * dZ_i);
-  const ST lap_i = lcart[ji] + mKK_ptr[index] * val_i - two * (kX * dX_r + kY * dY_r + kZ * dZ_r);
-
   TT* restrict psi    = results_scratch_ptr;
   TT* restrict dpsi_x = results_scratch_ptr + orb_padded_size;
   TT* restrict dpsi_y = results_scratch_ptr + orb_padded_size * 2;
   TT* restrict dpsi_z = results_scratch_ptr + orb_padded_size * 3;
   TT* restrict d2psi  = results_scratch_ptr + orb_padded_size * 4;
 
-  psi[index]    = TT(c * val_r - s * val_i, c * val_i + s * val_r);
-  d2psi[index]  = TT(c * lap_r - s * lap_i, c * lap_i + s * lap_r);
-  dpsi_x[index] = TT(c * gX_r - s * gX_i, c * gX_i + s * gX_r);
-  dpsi_y[index] = TT(c * gY_r - s * gY_i, c * gY_i + s * gY_r);
-  dpsi_z[index] = TT(c * gZ_r - s * gZ_i, c * gZ_i + s * gZ_r);
+  apply_phase_vgl(x, y, z, val_r, val_i, g0[jr], g0[ji], g1[jr], g1[ji], g2[jr], g2[ji], lcart[jr],
+                  lcart[ji], G, kX, kY, kZ, mKK_ptr[index], psi[index], dpsi_x[index], dpsi_y[index],
+                  dpsi_z[index], d2psi[index]);
 }
 } // namespace C2C
 } // namespace qmcplusplus
